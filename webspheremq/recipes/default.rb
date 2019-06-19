9f12c303d127feb5e4d42752b493a9d02ba9e13c
TITUS TONY <titus.tony@gmail.com>
2019-06-19 16:02:27 +0000

    Updated default.rb recipe

--- webspheremq/recipes/default.rb
+++ webspheremq/recipes/default.rb
@@ -1,5 +1,13 @@
 #
 # Cookbook:: webspheremq
 # Recipe:: default
+
+#include_recipe 'webspheremq::install_client_rhel' if rhel?
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+file '/tmp/test' do
+  content '<html>This is a placeholder for the home page.</html>'
+  mode '0755'
+  owner 'titus_alphonse_t'
+  group 'titus_alphonse_t'
+end
+#
